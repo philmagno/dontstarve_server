a1b8a789d48b1599d889c0908b960b05c73e4f00
Rafael Oliveira <rafael.oliveira@hotmart.com.br>
2018-06-28 14:02:28 -0300

    Update modoverrides.lua

--- modoverrides.lua
+++ modoverrides.lua
@@ -1,6 +1,11 @@
 return {
 	['workshop-378160973'] = { enabled = true },
 	['workshop-492173795'] = { enabled = true },
+	['workshop-666155465'] = { enabled = true },
+	['workshop-358749986'] = { enabled = true },
+	['workshop-347360448'] = { enabled = true },
+	['workshop-378160973'] = { enabled = true },
+	['workshop-462469447'] = { enabled = true },
 	['workshop-407705132'] = { enabled = true,
 		configuration_options = {
 			title = 'Welcome!',
